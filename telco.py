015c06e0b564251e613681102c190d780c35af63
Joseph Wang <joseph4x4@gmail.com>
2023-04-10 03:58:39 -0400

    Create telco.py
    Create a unified way to isolate data for evaluation across different models.

---
+++ telco.py
@@ -0,0 +1,49 @@
+from collections import namedtuple
+
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+class Dataset:
+    def __init__(self, excel="Telco_customer_churn.xlsx"):
+        # Load data
+        df = pd.read_excel(excel)
+
+        # Remove unrelated columns
+        df.drop(columns=[
+            "CustomerID", "Count", "Country", "State", "City", "Zip Code",
+            "Lat Long", "Latitude", "Longitude", "Churn Label", "Churn Score",
+            "CLTV", "Churn Reason"
+        ], inplace=True)
+
+        # Remove rows with empty values
+        df = df.loc[df["Total Charges"].strip() != ""]
+
+        # Isolate Features and Response
+        X = df.drop(columns=["Churn Value"])
+        Y = df["Churn Value"]
+        onehot_X = pd.get_dummies(X, drop_first=True)
+
+        # Remove evaluation set
+        SplitData = namedtuple("SplitData", [
+            "X", "X_eval", "ohX", "ohX_eval", "y", "y_eval"
+        ])
+        self._data = SplitData(*train_test_split(
+            X, onehot_X, Y, test_size=0.1, random_state=441
+        ))
+
+    def get_visible_set(self, onehot=False):
+        if onehot:
+            return self._data.ohX, self._data.y
+        else:
+            return self._data.X, self._data.y
+
+    def get_evaluation_set(self, onehot=False):
+        if onehot:
+            return self._data.ohX_eval
+        else:
+            return self._data.X_eval
+
+    def accuracy(self, y_pred):
+        return accuracy_score(self._data.y_eval, y_pred)
